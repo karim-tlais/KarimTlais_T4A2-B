a11f37f38141cfb4bf6aa542e20e5a3df1956df3
Karim Tlais <karimtls@icloud.com>
2021-03-07 14:55:55 +1100

    created database , created html and css templates + static directory containing bottstrap.min.css

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:1962@localhost/quotes'
+app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 @app.route('/')
 def index():
-    return render_template('index.html',quote='kindness needs no translation') 
-    
-@app.route('/about')
-def about():
-    return '<h1> Hello World from about page</h1>'
+    return render_template('index.html')
 
 @app.route('/quotes')
 def quotes():
-    return '<h1> Life is a journey</h1>'
+    return render_template('quotes.html')
+
+
+@app.route('/process',methods = ['POST'])
+def process():
+    author = request.form[ 'author']
+    quote = request.form['quote']
+    return redirect(url_for('index.html'))
