d3d1c1f8d533b018d543c27570c56ac4d0804716
Karim Tlais <karimtls@icloud.com>
2021-03-07 13:08:55 +1100

    Added template folder and index.html file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> Hello World</h1>'
+    return render_template('index.html',quote='kindness needs no translation') 
     
+@app.route('/about')
+def about():
+    return '<h1> Hello World from about page</h1>'
+
+@app.route('/quotes')
+def quotes():
+    return '<h1> Life is a journey</h1>'
