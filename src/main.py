6468b692cae398ced85dea02e3b4c3c7d153c434
Karim Tlais <karimtls@icloud.com>
2021-03-12 22:06:28 +1100

    fixed code

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://postgres:1962@localhost/diary'
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://cnotxobzwgzncz:42c1670ab07b580fdbac2ebde20f5fd35650dde3397ca355e0a6300cd5e0e2de@ec2-54-145-249-177.compute-1.amazonaws.com:5432/dejnqbve7qnjdq'
+app.config['SQLALCHEMY_DATABASE_URL'] ='postgres://cnotxobzwgzncz:42c1670ab07b580fdbac2ebde20f5fd35650dde3397ca355e0a6300cd5e0e2de@ec2-54-145-249-177.compute-1.amazonaws.com:5432/dejnqbve7qnjdq'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 db = SQLAlchemy(app)
