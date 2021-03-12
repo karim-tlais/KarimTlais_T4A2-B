c0e6a22a96771725703b5d933a7cccd7ffbecf33
Karim Tlais <karimtls@icloud.com>
2021-03-12 20:55:27 +1100

    created postgres database in heroku

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://postgres:1962@localhost/diary'
+# app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://postgres:1962@localhost/diary'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://lyusbfspcfviju:d831a4b81bc9802a6f1a0aec98096292fd892339a0cb864cc1a47875fcafb8b9@ec2-54-145-249-177.compute-1.amazonaws.com:5432/dcdsp9c5kjs7q5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 db = SQLAlchemy(app)
