cb7b38530b837726e2022f052a58bee946795909
Karim Tlais <karimtls@icloud.com>
2021-03-12 21:48:24 +1100

    updated postgres URL

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://postgres:1962@localhost/diary'
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://lyusbfspcfviju:d831a4b81bc9802a6f1a0aec98096292fd892339a0cb864cc1a47875fcafb8b9@ec2-54-145-249-177.compute-1.amazonaws.com:5432/dcdsp9c5kjs7q5'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://cnotxobzwgzncz:42c1670ab07b580fdbac2ebde20f5fd35650dde3397ca355e0a6300cd5e0e2de@ec2-54-145-249-177.compute-1.amazonaws.com:5432/dejnqbve7qnjdq'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 db = SQLAlchemy(app)
